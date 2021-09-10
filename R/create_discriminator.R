673fa9cd81ec86588ced3e77c9cfebe268f6a5f1
mneunhoe <mneunhoe@mail.uni-mannheim.de>
2021-09-10 19:03:54 +0200

    Added Description and License

--- R/create_discriminator.R
+++ R/create_discriminator.R
@@ -1,3 +1,15 @@
+#' Create a GAN Discriminator
+#'
+#' Create a GAN discriminator. You can either input your own architecture or rely on the default definition.
+#'
+#' @param a Input a
+#' @param b Input b
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' create_discriminator(a = "A", b = "B")
 create_discriminator <- function(a = "Hello", b = "World") {
   paste(a, b)
 }
